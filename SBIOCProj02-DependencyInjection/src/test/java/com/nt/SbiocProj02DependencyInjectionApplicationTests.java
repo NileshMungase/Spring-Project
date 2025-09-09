a201a1d2cdd0365f6a8f91ac937d91f945bc9030
ACER <ACER@Nilesh>
2025-09-09 22:27:17 +0530

    SBProj

---
+++ SBIOCProj02-DependencyInjection/src/test/java/com/nt/SbiocProj02DependencyInjectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbiocProj02DependencyInjectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
